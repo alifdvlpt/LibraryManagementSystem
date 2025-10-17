be75599aacfe78a1990164a33ddcfaf153a386e7
alifdvlpt <152212098+alifdvlpt@users.noreply.github.com>
2025-10-17 19:22:35 +0400

    Assignment

--- Library.py
+++ Library.py
@@ -45,7 +45,7 @@ class Payment(ABC):
 
 class CreditCardPayment(Payment):
     def pay(self, amount):
-        print(f"Paid ${amount} using Credit Card.")
+        print(f"Paid ${amount} using your Credit Card.")
 
 
 class CashPayment(Payment):
